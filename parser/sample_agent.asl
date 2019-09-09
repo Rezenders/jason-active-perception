89116eb6a1c0e80afd3d4f8044dcb3291b2fda70
Gustavo Rezende <gustavorezendesilva@hotmail.com>
2019-09-09 20:30:35 -0300

    🚧 Parser under construction

--- parser/sample_agent.asl
+++ parser/sample_agent.asl
@@ -3,28 +3,35 @@
 /* Initial beliefs and rules */
 e.
 /* Initial goals */
+!z.
 +e <- !g.
 
 /* Plans */
-+!g <- !update_ap_g; !g_ap.
+// +!g <- !update_ap_g; !g_ap.
+//
+// +!update(U) <- active_perception.update(U).
+//
+// +!update_ap_g
+// 	<- 	!update(b[ap(1000)]) |&|
+// 		!update(c[ap(2000)]) |&|
+// 		!update(d[ap(3000)]).
 
-+!update(U) <- active_perception.update(U).
++!z <- .print("teste").
 
-+!update_ap_g
-	<- 	!update(b[ap(1000)]) |&|
-		!update(c[ap(2000)]) |&|
-		!update(d[ap(3000)]).
++!g[ap(8888)] <- .print("oi").
 
-+!g_ap: b[ap(1000)] & c[ap(2000)] & d[ap(3000)]
++!g: b[ap(1000)] & c[ap(2000)] & d[ap(3000)]
 	<- .print("GOAL G1").
 
-+!g_ap: b[ap(1000)] & c[ap(2000)]
++!g: b[ap(1000)] & c[ap(2000)]
 	<- .print("GOAL G2").
 
-+!g_ap: b[ap(1000)]
++!g: b[ap(1000)]
 	<- .print("GOAL G3").
 
-+!g_ap <- .print("GOAL G4!").
++!g: h <- .print("GOAL G4!").
+
++!g <- .print("GOAL G5!").
 
 +?b[ap(T)]
 	<-	.time(HH,MM,SS);
--- parser/src/java/active_perception/ApplyAP.java
+++ parser/src/java/active_perception/ApplyAP.java
@@ -4,15 +4,63 @@ import jason.asSemantics.Agent;
 import jason.asSyntax.*;
 import jason.asSyntax.directives.*;
 import jason.asSyntax.BodyLiteral.BodyType;
+import static jason.asSyntax.ASSyntax.*;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.List;
+import java.util.ArrayList;
 
 public class ApplyAP extends DefaultDirective implements Directive {
 
 	public Agent process(Pred directive, Agent outerContent, Agent innerContent) {
-		System.out.println(outerContent.getPL());
+		// System.out.println(outerContent.getPL());
 		if (outerContent == null)
             return null;
-		// Agent newAg = new Agent();
+		Agent newAg = new Agent();
+		Set<Trigger> triggers_ap = new HashSet<Trigger>();
+		for(Plan p : outerContent.getPL()){
+			Literal context = (Literal)p.getContext();
+			if(context != null){
+				System.out.println(context);
+				System.out.println(p.getTrigger());
+				if(isAP(context)){
+					triggers_ap.add(p.getTrigger());
+				}
+			}
+		}
+
+		List<Plan> plans = new ArrayList<Plan>();
+		// //trocar +!g.... por +!g <- !update_ap_g; !g_ap.
+		for(Trigger t : triggers_ap){
+			//Plan​(Pred label, Trigger te, LogicalFormula ct, PlanBody bd)
+			Plan p = new Plan();
+			// new Plan( new Pred((Literal)(lt.get(0))),
+            //     (Trigger)lt.get(1),
+            //     (LogicalFormula)c,
+            //     (PlanBody)lt.get(3));
+			System.out.println(p.getLabel());
+		// 	p.
+		}
+		//adicionar +!g[ap(ALGUMA COISA)]
+
+
 		return null;
+	}
+
+	//Retornar os beliefs que precisam ser atualizados ao invés de true/false
+	boolean isAP(Literal context){
+		Integer arity = context.getArity();
+		if(arity==0 && !context.getAnnots("ap").isEmpty()){
+			return true;
+		}else if(arity > 0){
+			if(isAP((Literal)context.getTermsArray()[0])){
+				return true;
+			}
+			if(isAP((Literal)context.getTermsArray()[1])){
+				return true;
+			}
+		}
+		return false;
 	}
 
 }
