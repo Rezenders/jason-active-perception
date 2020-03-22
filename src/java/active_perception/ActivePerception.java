fecf5b8a8aee1cf34bf137323dc33bafbc8b12a1
Gustavo Rezende <gustavo.rezende@fieb.org.br>
2020-03-22 16:20:31 -0300

    ✨ Add AP common functionalities

--- src/java/active_perception/ActivePerception.java
+++ src/java/active_perception/ActivePerception.java
@@ -12,14 +12,9 @@ import java.util.HashMap;
 
 public class ActivePerception{
 
-	private Map<Trigger, LinkedHashSet<Literal>> ap_beliefs_map = new HashMap<Trigger, LinkedHashSet<Literal>>();
-
-	public ActivePerception(Agent outerContent){
-		findPlansApBel(outerContent);
-	}
 
 	///Get beliefs marked with ap
-	private List<Literal> findApBeliefs(Literal context){
+	private static List<Literal> findApBeliefs(Literal context){
 		List<Literal> beliefs = new ArrayList<Literal>();
 		Integer arity = context.getArity();
 
@@ -37,7 +32,8 @@ public class ActivePerception{
 	}
 
 	//Get plans that has any belief marked with ap in context
-	public void findPlansApBel(Agent outerContent){
+	public static Map<Trigger, LinkedHashSet<Literal>> getPlansApBel(Agent outerContent){
+		Map<Trigger, LinkedHashSet<Literal>> ap_beliefs_map = new HashMap<Trigger, LinkedHashSet<Literal>>();
 		for(Plan p : outerContent.getPL()){
 			Literal context = (Literal)p.getContext();
 			if(context != null){
@@ -54,9 +50,44 @@ public class ActivePerception{
 			}
 
 		}
-	}
-
-	public Map<Trigger, LinkedHashSet<Literal>> getPlansApBel(){
 		return ap_beliefs_map;
 	}
+
+	//Annotating inital goals with annot
+	public static void annotInitGoals(Agent outerContent, Map<Trigger, LinkedHashSet<Literal>> ap_beliefs_map, Atom annot){
+		for(Literal l: outerContent.getInitialGoals()){
+			for(Trigger t: ap_beliefs_map.keySet()){
+				if(t.getLiteral().getPredicateIndicator().equals(l.getPredicateIndicator())){
+					l.addAnnot(annot);
+				}
+			}
+		}
+	}
+
+	//Annotating plans with annot
+	public static void annotTriggers(Agent outerContent, Map<Trigger, LinkedHashSet<Literal>> ap_beliefs_map, Atom annot){
+		for(Plan p: outerContent.getPL()){
+			if(ap_beliefs_map.keySet().contains(p.getTrigger())){
+				p.getTrigger().getLiteral().addAnnots(annot);
+			}
+		}
+	}
+
+	//add [annot] to !g inside plans body
+	public static void annotPlanBodyAchieve(Agent outerContent, Map<Trigger, LinkedHashSet<Literal>> ap_beliefs_map, Atom annot){
+		for(Plan p: outerContent.getPL()){
+			PlanBody pb = p.getBody();
+			for(int i=0; i<p.getBody().getPlanSize(); i++){
+				Literal pb_ap = (Literal)pb.getBodyTerm();
+				for(Trigger t: ap_beliefs_map.keySet()){
+					if(t.getLiteral().getPredicateIndicator().equals(pb_ap.getPredicateIndicator()) && pb.getBodyType() == jason.asSyntax.PlanBody.BodyType.achieve ){
+						pb_ap.addAnnot(annot);
+						break;
+					}
+				}
+				pb = pb.getBodyNext();
+			}
+		}
+	}
+
 }
